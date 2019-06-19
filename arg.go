c824ea7c82763d0e1e3e04149fa845562fe0b183
kokihonma <kouki.honma@gmail.com>
2019-06-19 23:07:25 +0900

    子プロセスの起動処理を追加

--- arg.go
+++ arg.go
@@ -6,7 +6,7 @@ import (
 
 func parseArgs() (string, string, []string) {
 	logDir := flag.String(
-		"logdir", "", "Log output directory (default=stderr)"
+	"logdir", "", "Log output directory (default=stderr)"
 	)
 	flag.Parse()
 	return *logDir, flag.Arg(0), flag.Args()[1:]
